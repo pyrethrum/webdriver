8dac2c806525ff4407d31273db956ebb69f7c984
John Walker <theghostjw@gmail.com>
2025-02-28 12:08:03 +1100

    WIP

--- webdriver-precore/src/WebDriverPreCore/Spec.hs
+++ webdriver-precore/src/WebDriverPreCore/Spec.hs
@@ -5,35 +5,15 @@
 -- Here is a longer description of this module, containing some
 -- commentary with @some markup@.
 module WebDriverPreCore.Spec
-  ( --- types
-    Cookie (..),
-    DriverStatus (..),
-    ElementId (..),
-    FrameReference (..),
-    HttpResponse (..),
-    SameSite (..),
-    Selector (..),
-    SessionId (..),
-    Timeouts (..),
+  ( 
+    -- * The W3Spec Type
     W3Spec (..),
-    WindowHandle (..),
-    WindowHandleSpec (..),
-    WindowRect (..),
-    UrlPath (..),
-    --- action types
-    Action (..),
-    Actions (..),
-    KeyAction (..),
-    Pointer (..),
-    PointerAction (..),
-    PointerOrigin (..),
-    WheelAction (..),
-    --- Specs
-    -- Root Methods
+    -- * The API
+    -- ** Root Methods
     newSession,
     newSession',
     status,
-    -- Session Methods
+    -- ** Session Methods
     deleteSession,
     getTimeouts,
     setTimeouts,
@@ -64,20 +44,20 @@ module WebDriverPreCore.Spec
     sendAlertText,
     takeScreenshot,
     printPage,
-    -- Window Methods
+    -- ** Window Methods
     getWindowHandles,
     getWindowRect,
     setWindowRect,
     maximizeWindow,
     minimizeWindow,
     fullscreenWindow,
-    -- Frame Methods
+    -- ** Frame Methods
     switchToParentFrame,
-    -- Element(s) Methods
+    -- ** Element(s) Methods
     getActiveElement,
     findElement,
     findElements,
-    -- Element Instance Methods
+    -- ** Element Instance Methods
     getElementShadowRoot,
     findElementFromElement,
     findElementsFromElement,
@@ -95,9 +75,34 @@ module WebDriverPreCore.Spec
     elementClear,
     elementSendKeys,
     takeElementScreenshot,
-    -- Shadow DOM Methods
+    -- ** Shadow DOM Methods
     findElementFromShadowRoot,
     findElementsFromShadowRoot,
+    
+    -- * Auxilary Types
+    Cookie (..),
+    DriverStatus (..),
+    ElementId (..),
+    FrameReference (..),
+    HttpResponse (..),
+    SameSite (..),
+    Selector (..),
+    SessionId (..),
+    Timeouts (..),
+    WindowHandle (..),
+    WindowHandleSpec (..),
+    WindowRect (..),
+    UrlPath (..),
+
+    -- ** Action Types
+    Action (..),
+    Actions (..),
+    KeyAction (..),
+    Pointer (..),
+    PointerAction (..),
+    PointerOrigin (..),
+    WheelAction (..),
+    
   )
 where
 
@@ -289,99 +294,51 @@ data Selector
   | TagName Text
   deriving (Show, Eq)
 
+
 -- ######################################################################
 -- ########################### WebDriver API ############################
 -- ######################################################################
-{- FUll List
-https://w3c.github.io/webdriver/#endpoints - W3C Editor's Draft 10 February 2025
-61 endpoints
-Method 	URI Template 	Command
-POST 	/session 	New Session
-DELETE 	/session/{session id} 	Delete Session
-GET 	/status 	Status
-GET 	/session/{session id}/timeouts 	Get Timeouts
-POST 	/session/{session id}/timeouts 	Set Timeouts
-POST 	/session/{session id}/url 	Navigate To
-GET 	/session/{session id}/url 	Get Current URL
-POST 	/session/{session id}/back 	Back
-POST 	/session/{session id}/forward 	Forward
-POST 	/session/{session id}/refresh 	Refresh
-GET 	/session/{session id}/title 	Get Title
-GET 	/session/{session id}/window 	Get Window Handle
-DELETE 	/session/{session id}/window 	Close Window
-POST 	/session/{session id}/window 	Switch To Window
-GET 	/session/{session id}/window/handles 	Get Window Handles
-POST 	/session/{session id}/window/new 	New Window
-POST 	/session/{session id}/frame 	Switch To Frame
-POST 	/session/{session id}/frame/parent 	Switch To Parent Frame
-GET 	/session/{session id}/window/rect 	Get Window Rect
-POST 	/session/{session id}/window/rect 	Set Window Rect
-POST 	/session/{session id}/window/maximize 	Maximize Window
-POST 	/session/{session id}/window/minimize 	Minimize Window
-POST 	/session/{session id}/window/fullscreen 	Fullscreen Window
-GET 	/session/{session id}/element/active 	Get Active Element
-GET 	/session/{session id}/element/{element id}/shadow 	Get Element Shadow Root
-POST 	/session/{session id}/element 	Find Element
-POST 	/session/{session id}/elements 	Find Elements
-POST 	/session/{session id}/element/{element id}/element 	Find Element From Element
-POST 	/session/{session id}/element/{element id}/elements 	Find Elements From Element
-POST 	/session/{session id}/shadow/{shadow id}/element 	Find Element From Shadow Root
-POST 	/session/{session id}/shadow/{shadow id}/elements 	Find Elements From Shadow Root
-GET 	/session/{session id}/element/{element id}/selected 	Is Element Selected
-GET 	/session/{session id}/element/{element id}/attribute/{name} 	Get Element Attribute
-GET 	/session/{session id}/element/{element id}/property/{name} 	Get Element Property
-GET 	/session/{session id}/element/{element id}/css/{property name} 	Get Element CSS Value
-GET 	/session/{session id}/element/{element id}/text 	Get Element Text
-GET 	/session/{session id}/element/{element id}/name 	Get Element Tag Name-- [API Docs](https://www.w3.org/TR/2025/WD-webdriver2-20250210/#new-session)
-GET 	/session/{session id}/element/{element id}/rect 	Get Element Rect
-GET 	/session/{session id}/element/{element id}/enabled 	Is Element Enabled
-GET 	/session/{session id}/element/{element id}/computedrole 	Get Computed Role
-GET 	/session/{session id}/element/{element id}/computedlabel 	Get Computed Label
-POST 	/session/{session id}/element/{element id}/click 	Element Click
-POST 	/session/{session id}/element/{element id}/clear 	Element Clear
-POST 	/session/{session id}/element/{element id}/value 	Element Send Keys
-GET 	/session/{session id}/source 	Get Page Source
-POST 	/session/{session id}/execute/sync 	Execute Script
-POST 	/session/{session id}/execute/async 	Execute Async Script
-GET 	/session/{session id}/cookie 	Get All Cookies
-GET 	/session/{session id}/cookie/{name} 	Get Named Cookie
-POST 	/session/{session id}/cookie 	Add Cookie
-DELETE 	/session/{session id}/cookie/{name} 	Delete Cookie
-DELETE 	/session/{session id}/cookie 	Delete All Cookies
-POST 	/session/{session id}/actions 	Perform Actions
-DELETE 	/session/{session id}/actions 	Release Actions
-POST 	/session/{session id}/alert/dismiss 	Dismiss Alert
-POST 	/session/{session id}/alert/accept 	Accept Alert
-GET 	/session/{session id}/alert/text 	Get Alert Text
-POST 	/session/{session id}/alert/text 	Send Alert Text
-GET 	/session/{session id}/screenshot 	Take Screenshot
-GET 	/session/{session id}/element/{element id}/screenshot 	Take Element Screenshot
-POST 	/session/{session id}/print 	Print Page
--}
 
--- https://www.w3.org/TR/2024/WD-webdriver2-20241212/#endpoints
+
+-- https://www.w3.org/TR/2025/WD-webdriver2-20250210/
 -- 61 endpoints
 -- Method 	URI Template 	Command
 
--- ############################ Root Methods ##########################################
+-- ** Root Methods
 
 -- |
 --
---  The 'newSession' function returns a 'W3Spec' corresponding to the following WebDriver [API](https://www.w3.org/TR/2025/WD-webdriver2-20250210/#new-session) endpoint:
+--  'newSession' returns a 'W3Spec' corresponding to the following WebDriver [WebDriver endpoint](https://www.w3.org/TR/2025/WD-webdriver2-20250210/#new-session)
 --
 --  @POST 	/session 	New Session@
 --
+--  Return a spec to create a new session given 'FullCapabilities' object.
 --
---
---  Return a spec to create a new session with the given 'FullCapabilities'.
+-- 'newSession'' can be used if 'FullCapabilities' doesn't meet your requirements.
 newSession :: FullCapabilities -> W3Spec SessionId
 newSession = newSession'
 
--- POST 	/session 	New Session
+-- |
+--
+--  'newSession'' returns a 'W3Spec' corresponding to the following [WebDriver endpoint](https://www.w3.org/TR/2025/WD-webdriver2-20250210/#new-session)
+--
+--  @POST 	/session 	New Session@
+--
+--  Return a spec to create a new session given an object of any type that implements `ToJSON`.
+--
+-- 'FullCapabilities' type and associated types should fit the requirments for the vast majority use cases but there may be edge cases (such as missing 'Capabilities.VendorSpecific' capbilites) 
+-- where 'FullCapabilities' are not sufficient. 'newSession'' is a fallback for such cases and can be used with any user defined type that implments 'ToJSON', (including an Aeson 'Value'). 
+-- Obviously, any type used must be compatiable with the [Capabilities](https://www.w3.org/TR/2025/WD-webdriver2-20250210/#capabilities) spec.
 newSession' :: (ToJSON a) => a -> W3Spec SessionId
-newSession' capabilities = Post "New Session" (MkUrlPath [session] )(toJSON capabilities) parseSessionRef
+newSession' capabilities = Post "New Session" (MkUrlPath [session]) (toJSON capabilities) parseSessionRef
 
--- GET 	/status 	Status
+-- |
+--
+-- 'status' returns a 'W3Spec' corresponding to the following [WebDriver endpoint](https://www.w3.org/TR/2025/WD-webdriver2-20250210/#status)
+--
+-- @GET 	/status 	Status@
+--
+-- Return a spec to get the status of the driver.
 status :: W3Spec DriverStatus
 status = Get "Status" (MkUrlPath ["status"]) parseDriverStatus
 
@@ -837,7 +794,6 @@ elemtRefFromBody b = Element <$> lookupTxt elementFieldName b
 session :: Text
 session = "session"
 
-
 sessionUri :: Text -> UrlPath
 sessionUri sp = MkUrlPath [session, sp]
 
@@ -863,7 +819,7 @@ elementUri1 :: SessionId -> ElementId -> Text -> UrlPath
 elementUri1 s er ep = sessionUri3 s "element" er.id ep
 
 elementUri2 :: SessionId -> ElementId -> Text -> Text -> UrlPath
-elementUri2 s er ep ep2 = sessionUri4 s "element" er.id ep  ep2
+elementUri2 s er ep ep2 = sessionUri4 s "element" er.id ep ep2
 
 jsonPrettyString :: Value -> String
 jsonPrettyString = unpack . jsonToText
