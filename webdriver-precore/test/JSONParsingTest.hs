466c7c67ad01bc9d196bdb23ba68b85aaea3e863
John Walker <theghostjw@gmail.com>
2025-03-05 14:45:46 +1100

    WIP

--- webdriver-precore/test/JSONParsingTest.hs
+++ webdriver-precore/test/JSONParsingTest.hs
@@ -6,81 +6,6 @@ import GHC.Utils.Misc (filterOut)
 import Test.Tasty.HUnit as HUnit ( assertBool, Assertion, (@=?) )
 import Text.RawString.QQ (r)
 import WebDriverPreCore.Spec
-    ( SessionId(Session),
-      ElementId(Element),
-      Selector(CSS),
-      WindowHandle(Handle),
-      WindowRect(Rect),
-      FrameReference(TopLevelFrame),
-      Cookie(MkCookie),
-      Actions(MkActions),
-      Timeouts(..),
-      W3Spec(description, Get, Post, PostEmpty, Delete, path),
-      errorTypeToErrorCode,
-      minFirefoxCapabilities,
-      errorDescription,
-      acceptAlert,
-      addCookie,
-      back,
-      closeWindow,
-      deleteAllCookies,
-      deleteCookie,
-      deleteSession,
-      dismissAlert,
-      elementClear,
-      elementClick,
-      elementSendKeys,
-      errorCodeToErrorType,
-      executeScript,
-      executeScriptAsync,
-      findElement,
-      findElementFromElement,
-      findElementFromShadowRoot,
-      findElements,
-      findElementsFromElement,
-      findElementsFromShadowRoot,
-      forward,
-      fullscreenWindow,
-      getActiveElement,
-      getAlertText,
-      getAllCookies,
-      getCurrentUrl,
-      getElementAttribute,
-      getElementComputedLabel,
-      getElementComputedRole,
-      getElementCssValue,
-      getElementProperty,
-      getElementRect,
-      getElementShadowRoot,
-      getElementTagName,
-      getElementText,
-      getNamedCookie,
-      getPageSource,
-      getTimeouts,
-      getTitle,
-      getWindowHandle,
-      getWindowHandles,
-      getWindowRect,
-      isElementEnabled,
-      isElementSelected,
-      maximizeWindow,
-      minimizeWindow,
-      navigateTo,
-      newSession,
-      newWindow,
-      performActions,
-      printPage,
-      refresh,
-      releaseActions,
-      sendAlertText,
-      setTimeouts,
-      setWindowRect,
-      status,
-      switchToFrame,
-      switchToParentFrame,
-      switchToWindow,
-      takeElementScreenshot,
-      takeScreenshot )
 import GHC.Show (Show (..))
 import Data.Eq (Eq ((==)))
 import Data.Ord (Ord)
@@ -94,5 +19,62 @@ import WebDriverPreCore.Internal.Utils (enumerate)
 import Data.Foldable (traverse_)
 import Data.Either (either)
 import WebDriverPreCore.Spec (UrlPath(..))
+
+import Test.Falsify.Generator as G (Gen, frequency, inRange, list)
+import Test.Falsify.Predicate qualified as FP
+import Test.Falsify.Range as R (between, skewedBy, enum) 
+import Test.Tasty (TestName, TestTree, defaultMain, testGroup)
+import Test.Tasty.Falsify
+
+import Test.Falsify.Generator as G
+import Data.Aeson
+import Data.Enum (Enum, Bounded (minBound), maxBound)
+import GHC.Err (undefined)
+import Control.Applicative (pure)
+
+
+
+-- Generate a random Value (very simplistic)
+genJSONValue :: G.Gen Value
+genJSONValue = G.frequency
+  [ (1, pure A.Null)
+  , (1, pure $ object ["key" .= ("example" :: Text)])
+  ]
+
+
+genOneOf :: forall a. Enum a => G.Gen a
+genOneOf = G.inRange $ R.enum (minBound @a, maxBound @a)
+
+genMaybeOf :: forall a. Enum a => G.Gen (Maybe a)
+genMaybeOf gen' = G.frequency
+  [ (1, pure Nothing)
+  , (3, Just <$> gen')
+  ]
+
+-- Generate a random BrowserName
+-- genBrowserName :: G.Gen BrowserName
+-- genBrowserName = genOneOf
+
+-- Generate a random UnhandledPromptBehavior
+genUnhandledPromptBehavior :: G.Gen UnhandledPromptBehavior
+genUnhandledPromptBehavior = G.frequency
+  [ (1, pure Dismiss)
+  , (1, pure Accept)
+  , (1, pure DismissAndNotify)
+  , (1, pure AcceptAndNotify)
+  , (1, pure Ignore)
+  ]
+
+-- Generate random Capabilities
+genCapabilities :: G.Gen Capabilities
+genCapabilities = do
+
+  pure $ undefined
+    MkCapabilities
+      { browserName = mbBrowser
+      , strictFileInteractability = mbStrictFileInteract
+      , unhandledPromptBehavior = mbUnhandledPrompt
+      , vendorSpecific = mbVendorSpecific
+      }
 
 
