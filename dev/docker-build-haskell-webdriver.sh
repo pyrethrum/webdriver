c45369940f2fed03cc2e30e328ce7fe4fba9140f
John Walker <theghostjw@gmail.com>
2025-04-15 08:50:49 +1000

    bas docker file

--- dev/docker-build-haskell-webdriver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-docker build --tag haskell-webdriver --file dockerfile-haskell-webdriver .
-docker tag haskell-webdriver haskell-webdriver:latest
