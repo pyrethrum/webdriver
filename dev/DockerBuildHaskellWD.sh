005743722bb00047e81d4ae0bbb131b13313cf3d
John Walker <theghostjw@gmail.com>
2025-04-18 15:58:59 +1000

    remove old docker files

--- dev/DockerBuildHaskellWD.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-docker build --tag "haskell-webdriver" --file DockerfileHaskell-WebDriver .
-# docker tag haskell theghostjw/haskell:latest
-# docker push theghostjw/haskell:latest
