b12a17113b2b4b1ab2f2672e1b6d96b11c94cb25
John Walker <theghostjw@gmail.com>
2025-04-20 17:40:58 +1000

    again

--- container-start.sh
+++ container-start.sh
@@ -4,11 +4,8 @@
 pkill -f geckodriver || true
 
 # Start geckodriver in the background with logging
-geckodriver --log trace > /tmp/geckodriver.log 2>&1 &
+nohup geckodriver --log trace > /tmp/geckodriver.log 2>&1 &
 
 # Build the project
 cd webdriver-examples
 cabal build --enable-tests
-
-# Keep the script running to maintain the container
-tail -f /dev/null
