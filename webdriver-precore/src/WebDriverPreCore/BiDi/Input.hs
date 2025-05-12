2963ec8b83450b74aa6638c1c1bd488d15bf5d57
John Walker <theghostjw@gmail.com>
2025-05-12 20:46:52 +0000

    Clean up input

--- webdriver-precore/src/WebDriverPreCore/BiDi/Input.hs
+++ webdriver-precore/src/WebDriverPreCore/BiDi/Input.hs
@@ -1,18 +1,46 @@
-module WebDriverPreCore.BiDi.Input where
+module WebDriverPreCore.BiDi.Input
+  ( InputCommand (..),
+    PerformActions (..),
+    SourceActions (..),
+    NoneSourceActions (..),
+    KeySourceActions (..),
+    KeySourceAction (..),
+    PointerSourceActions (..),
+    PointerSourceAction (..),
+    WheelSourceActions (..),
+    WheelSourceAction (..),
+    PauseAction (..),
+    KeyDownAction (..),
+    KeyUpAction (..),
+    PointerUpAction (..),
+    PointerDownAction (..),
+    PointerMoveAction (..),
+    WheelScrollAction (..),
+    PointerCommonProperties (..),
+    Origin (..),
+    ReleaseActions (..),
+    SetFiles (..),
+    FileDialogOpened (..),
+    FileDialogInfo (..),
+    ElementOrigin (..),
+    Pointer (..),
+    PointerType (..)
+  )
+where
 
 import Data.Text (Text)
 import GHC.Generics (Generic)
 import WebDriverPreCore.BiDi.BrowsingContext qualified as BrowsingContext
 import WebDriverPreCore.BiDi.Script qualified as Script
-import Prelude (Show, Eq, Maybe, Int, Double, Bool)
+import Prelude (Bool, Double, Eq, Int, Maybe, Show)
 
--- Remote Types for Input module
+-- ######### Local #########
 
 -- Command types
-data InputCommand
-  = InputPerformActions PerformActions
-  | InputReleaseActions ReleaseActions
-  | InputSetFiles SetFiles
+data InputCommand = 
+   InputPerformActions PerformActions | 
+   InputReleaseActions ReleaseActions | 
+   InputSetFiles SetFiles
   deriving (Show, Eq, Generic)
 
 -- Element Origin
@@ -154,7 +182,7 @@ data PointerCommonProperties = MkPointerCommonProperties
   deriving (Show, Eq, Generic)
 
 data Origin
-  = ViewportOrigin
+  = ViewportOriginPointerType
   | PointerOrigin
   | ElementOriginRef ElementOrigin
   deriving (Show, Eq, Generic)
@@ -164,8 +192,6 @@ newtype ReleaseActions = MkReleaseActions
   { context :: BrowsingContext.BrowsingContextId
   }
   deriving (Show, Eq, Generic)
-
-
 
 data SetFiles = MkSetFiles
   { context :: BrowsingContext.BrowsingContextId,
@@ -179,206 +205,11 @@ data FileDialogOpened = MkFileDialogOpened
   }
   deriving (Show, Eq, Generic)
 
+-- ######### Local #########
+
 data FileDialogInfo = MkFileDialogInfo
   { context :: BrowsingContext.BrowsingContextId,
     element :: Maybe Script.SharedReference,
     multiple :: Bool
   }
   deriving (Show, Eq, Generic)
-
-{-
-
-Remote cddl
-
-InputCommand = (
-  input.PerformActions //
-  input.ReleaseActions //
-  input.SetFiles
-)
-
-input.ElementOrigin = {
-  type: "element",
-  element: script.SharedReference
-}
-
-input.PerformActions = (
-  method: "input.performActions",
-  params: input.PerformActions
-)
-
-input.PerformActions = {
-  context: browsingContext.BrowsingContext,
-  actions: [*input.SourceActions]
-}
-
-input.SourceActions = (
-  input.NoneSourceActions /
-  input.KeySourceActions /
-  input.PointerSourceActions /
-  input.WheelSourceActions
-)
-
-input.NoneSourceActions = {
-  type: "none",
-  id: text,
-  actions: [*input.NoneSourceAction]
-}
-
-input.NoneSourceAction = input.PauseAction
-
-input.KeySourceActions = {
-  type: "key",
-  id: text,
-  actions: [*input.KeySourceAction]
-}
-
-input.KeySourceAction = (
-  input.PauseAction /
-  input.KeyDownAction /
-  input.KeyUpAction
-)
-
-input.PointerSourceActions = {
-  type: "pointer",
-  id: text,
-  ? : input.Pointer,
-  actions: [*input.PointerSourceAction]
-}
-
-input.PointerType = "mouse" / "pen" / "touch"
-
-input.Pointer = {
-  ? pointerType: input.PointerType .default "mouse"
-}
-
-input.PointerSourceAction = (
-  input.PauseAction /
-  input.PointerDownAction /
-  input.PointerUpAction /
-  input.PointerMoveAction
-)
-
-input.WheelSourceActions = {
-  type: "wheel",
-  id: text,
-  actions: [*input.WheelSourceAction]
-}
-
-input.WheelSourceAction = (
-  input.PauseAction /
-  input.WheelScrollAction
-)
-
-input.PauseAction = {
-  type: "pause",
-  ? duration: js-uint
-}
-
-input.KeyDownAction = {
-  type: "keyDown",
-  value: text
-}
-
-input.KeyUpAction = {
-  type: "keyUp",
-  value: text
-}
-
-input.PointerUpAction = {
-  type: "pointerUp",
-  button: js-uint,
-}
-
-input.PointerDownAction = {
-  type: "pointerDown",
-  button: js-uint,
-  input.PointerCommonProperties
-}
-
-input.PointerMoveAction = {
-  type: "pointerMove",
-  x: float,
-  y: float,
-  ? duration: js-uint,
-  ? origin: input.Origin,
-  input.PointerCommonProperties
-}
-
-input.WheelScrollAction = {
-  type: "scroll",
-  x: js-int,
-  y: js-int,
-  deltaX: js-int,
-  deltaY: js-int,
-  ? duration: js-uint,
-  ? origin: input.Origin .default "viewport",
-}
-
-input.PointerCommonProperties = (
-  ? width: js-uint .default 1,
-  ? height: js-uint .default 1,
-  ? pressure: float .default 0.0,
-  ? tangentialPressure: float .default 0.0,
-  ? twist: (0..359) .default 0,
-  ; 0 .. Math.PI / 2
-  ? altitudeAngle: (0.0..1.5707963267948966) .default 0.0,
-  ; 0 .. 2 * Math.PI
-  ? azimuthAngle: (0.0..6.283185307179586) .default 0.0,
-)
-
-input.Origin = "viewport" / "pointer" / input.ElementOrigin
-
-input.ReleaseActions = (
-  method: "input.releaseActions",
-  params: input.ReleaseActions
-)
-
-input.ReleaseActions = {
-  context: browsingContext.BrowsingContext,
-}
-
-input.SetFiles = (
-  method: "input.setFiles",
-  params: input.SetFiles
-)
-
-input.SetFiles = {
-  context: browsingContext.BrowsingContext,
-  element: script.SharedReference,
-  files: [*text]
-}
-
-input.FileDialogOpened = (
-   method: "input.fileDialogOpened",
-   params: input.FileDialogInfo
-)
-
-input.FileDialogInfo = {
-   context: browsingContext.BrowsingContext,
-   ? element: script.SharedReference,
-   multiple: bool,
-}
-
--}
--- ######### Remote #########
-
-{-
-loacal cddl
-
-InputEvent = (
-  input.FileDialogOpened
-)
-
-input.FileDialogOpened = (
-   method: "input.fileDialogOpened",
-   params: input.FileDialogInfo
-)
-
-input.FileDialogInfo = {
-   context: browsingContext.BrowsingContext,
-   ? element: script.SharedReference,
-   multiple: bool,
-}
- -}
-
--- ######### Local #########
