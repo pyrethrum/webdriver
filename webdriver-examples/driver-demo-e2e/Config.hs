f5a9dab6ddeb7c17914c37ef2697016210bf23a5
John Walker <theghostjw@gmail.com>
2025-06-21 09:29:02 +0000

    finis config stuff

--- webdriver-examples/driver-demo-e2e/Config.hs
+++ webdriver-examples/driver-demo-e2e/Config.hs
@@ -50,7 +50,7 @@ configDir :: IO FilePath
 configDir = do
   currentDir <- getCurrentDirectory
   case findWebDriverRoot currentDir of
-    Just root -> pure $ root </> "webdriver-examples" </> "driver-demo-e2e" </> "config"
+    Just root -> pure $ root </> "webdriver-examples" </> "driver-demo-e2e" </> ".config"
     Nothing -> error "Could not find webdriver root directory"
 
 
