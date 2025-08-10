62324bfe16eae4a07fb397a23e501c91cec59c04
John Walker <theghostjw@gmail.com>
2025-08-10 14:58:05 +1000

    WIP

--- webdriver-precore/src/WebDriverPreCore/BiDi/Protocol.hs
+++ webdriver-precore/src/WebDriverPreCore/BiDi/Protocol.hs
@@ -2,14 +2,15 @@ module WebDriverPreCore.BiDi.Protocol where
 
 import Data.Aeson
   ( Object,
-    Value (..),
+    Value (..), ToJSON,
   )
-import WebDriverPreCore.BiDi.CoreTypes (JSUInt)
+import WebDriverPreCore.BiDi.Command as C (command, extendedCommand)
+import WebDriverPreCore.BiDi.CoreTypes (JSUInt, BiDiMethod)
 
-data Command c r = MkCommand {
-  command :: c -> JSUInt -> Value,
-  extended :: c -> Object -> JSUInt -> Value --,
-  -- responseMatcher :: JSUInt -> ResponseObject -> Either ResponseError (Maybe (MatchedResponse r))
-}
+data Command c r = MkCommand
+  { command :: c -> JSUInt -> Value,
+    extended :: c -> Object -> JSUInt -> Value
+  }
 
-
+command :: (BiDiMethod c, ToJSON c) => Command c r
+command = MkCommand {command = C.command, extended = extendedCommand}
