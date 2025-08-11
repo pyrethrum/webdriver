8776e04ecacfc277bb2c2d088072388e1cd4bc1f
John Walker <theghostjw@gmail.com>
2025-08-12 07:45:40 +1000

    remove old comments

--- webdriver-precore/src/WebDriverPreCore/BiDi/Command.hs
+++ webdriver-precore/src/WebDriverPreCore/BiDi/Command.hs
@@ -28,7 +28,6 @@ emptyCommand method = mkCommand method KM.empty
 setExtension :: Command c r -> Object -> Command c r
 setExtension MkCommand {method, params} extended = MkCommand {method, params, extended = Just extended}
 
--- TODO: check exceptions eg test with unsupported command - currently not getting to main thread
 commandValue :: (ToJSON c) => Command c r -> JSUInt -> Value
 commandValue MkCommand {method, params, extended} id' =
     object
@@ -38,23 +37,3 @@ commandValue MkCommand {method, params, extended} id' =
     ]
   where
     cmdObj = objectOrThrow "CommandData will always be an Object" params
-
--- command :: (ToJSON a) => Text -> a -> JSUInt -> Value
--- command method cmd = baseCommand method cmd Nothing
-
--- extendedCommand :: (ToJSON a) => Text -> a -> Object -> JSUInt -> Value
--- extendedCommand method cmd = baseCommand method cmd . Just
--- Command types
-
--- data Command
---   = BrowserCommand BrowserCommand
---   | BrowsingContext BrowsingContextCommand
---   | EmulationCommand EmulationCommand
---   | Input InputCommand
---   | Network NetworkCommand
---   | Script ScriptCommand
---   | Session SessionCommand
---   | Storage StorageCommand
---   | WebExtension WebExtensionCommand
---   deriving (Show, Eq, Generic)
-
