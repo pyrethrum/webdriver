891c05fb79eaccdbc8126b10a9f26414c36eb9c5
John Walker <theghostjw@gmail.com>
2025-12-23 08:37:45 +1100

    wip

--- dev/start-geckodriver.sh
+++ dev/start-geckodriver.sh
@@ -7,18 +7,9 @@ set -e
 pkill -x geckodriver || true
 
 PORT=4444
-WEBSOCKET_PORT=0 # 0 means a random port will be assigned
+# 0 means a random port will be assigned
+WEBSOCKET_PORT=0 
 MARIONETTE_PORT=2828
-
-# Set Firefox preferences
-# export MOZ_REMOTE_SETTINGS_DEV=1
-# export MOZ_ENABLE_WEBDRIVER=1
-
-# nohup geckodriver \
-#   --port $PORT \
-#   --websocket-port $WEBSOCKET_PORT \
-#   --binary "$(which firefox)" \
-#   --marionette-port $MARIONETTE_PORT > geckodriver.log 2>&1 &
 
 nohup geckodriver \
   --port $PORT \
@@ -27,14 +18,14 @@ nohup geckodriver \
 disown
 echo "geckodriver started with PID $! at http://127.0.0.1:$PORT"
 
-# Port checking alternatives
+# Wait for geckodriver to be ready before script exits
 echo "Checking port..."
-check_port() {
-  (timeout 1 bash -c '</dev/tcp/127.0.0.1/'$1) 2>/dev/null && \
+wait_for_port() {
+  (timeout 10 bash -c '</dev/tcp/127.0.0.1/'$1) 2>/dev/null && \
     echo "Port $1 is open" || \
     echo "Port $1 failed"
 }
 
-check_port $PORT
-# check_port $WEBSOCKET_PORT
+wait_for_port $PORT
+# wait_for_port $WEBSOCKET_PORT
 # websocket port 0 (dynamic) will always fail)
