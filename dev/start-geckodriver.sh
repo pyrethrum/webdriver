90c11e71b52374ac2d78dd122722e3c9a0c3bcbf
John Walker <theghostjw@gmail.com>
2026-01-06 18:30:20 +1100

    wip

--- dev/start-geckodriver.sh
+++ dev/start-geckodriver.sh
@@ -17,6 +17,7 @@ nohup geckodriver \
 
 disown
 echo "geckodriver started with PID $! at http://127.0.0.1:$PORT"
+geckodriver -V 
 
 # Wait for geckodriver to be ready before script exits
 echo "Checking port..."
