ec577a7bb2f1d2bd0d7b166b0822d92dfa073225
John Walker <theghostjw@gmail.com>
2026-01-24 16:18:47 +1100

    fix error demo

--- webdriver-precore/bidi-runner/test/ErrorDemo.hs
+++ webdriver-precore/bidi-runner/test/ErrorDemo.hs
@@ -26,7 +26,6 @@ import WebDriverPreCore.BiDi.Protocol
     SourceActions (..),
     ErrorType (..), 
     WebDriverException (..), 
-    SharedReference(..),
     parseWebDriverException
   )
 import Prelude hiding (log)
@@ -36,7 +35,6 @@ _rundemo :: BiDiDemo -> IO ()
 _rundemo = runDemo
 
 -- >>> runDemo errorDemo
--- *** Exception: ProtocolException {error = NoSuchNode, description = "Tried to deserialize an unknown SharedReference", message = "The node with the reference invalid-shared-id-that-does-not-exist is not known", stacktrace = Just "RemoteError@chrome://remote/content/shared/RemoteError.sys.mjs:8:8\nWebDriverError@chrome://remote/content/shared/webdriver/Errors.sys.mjs:202:5\nNoSuchNodeError@chrome://remote/content/shared/webdriver/Errors.sys.mjs:717:5\ndeserializeSharedReference@chrome://remote/content/webdriver-bidi/RemoteValue.sys.mjs:224:11\ndeserialize@chrome://remote/content/webdriver-bidi/RemoteValue.sys.mjs:275:12\ndeserialize@chrome://remote/content/webdriver-bidi/modules/WindowGlobalBiDiModule.sys.mjs:42:17\n#deserializeElementSharedReference@chrome://remote/content/webdriver-bidi/modules/windowglobal/input.sys.mjs:119:26\n_getClientRects@chrome://remote/content/webdriver-bidi/modules/windowglobal/input.sys.mjs:289:66\nhandleCommand@chrome://remote/content/shared/messagehandler/MessageHandler.sys.mjs:282:33\nreceiveMessage@chrome://remote/content/shared/messagehandler/transports/js-window-actors/MessageHandlerFrameChild.sys.mjs:78:37\n", errorData = Nothing, response = Object (fromList [("error",String "no such node"),("id",Number 3.0),("message",String "The node with the reference invalid-shared-id-that-does-not-exist is not known"),("stacktrace",String "RemoteError@chrome://remote/content/shared/RemoteError.sys.mjs:8:8\nWebDriverError@chrome://remote/content/shared/webdriver/Errors.sys.mjs:202:5\nNoSuchNodeError@chrome://remote/content/shared/webdriver/Errors.sys.mjs:717:5\ndeserializeSharedReference@chrome://remote/content/webdriver-bidi/RemoteValue.sys.mjs:224:11\ndeserialize@chrome://remote/content/webdriver-bidi/RemoteValue.sys.mjs:275:12\ndeserialize@chrome://remote/content/webdriver-bidi/modules/WindowGlobalBiDiModule.sys.mjs:42:17\n#deserializeElementSharedReference@chrome://remote/content/webdriver-bidi/modules/windowglobal/input.sys.mjs:119:26\n_getClientRects@chrome://remote/content/webdriver-bidi/modules/windowglobal/input.sys.mjs:289:66\nhandleCommand@chrome://remote/content/shared/messagehandler/MessageHandler.sys.mjs:282:33\nreceiveMessage@chrome://remote/content/shared/messagehandler/transports/js-window-actors/MessageHandlerFrameChild.sys.mjs:78:37\n"),("type",String "error")])}
 errorDemo :: BiDiDemo
 errorDemo =
   demo "BiDi Error Demo" action
@@ -116,20 +114,14 @@ expectProtocolException ::
   (HasCallStack) =>
   ErrorType ->
   IO a ->
-  IO ResponseException
+  IO WebDriverException
 expectProtocolException expectedError action =
   try action
     >>= \case
-      Left exc@(BiDIError errorValue) -> do
-        -- Parse the Value to get WebDriverException
-        let parsedExc = parseWebDriverException "expectProtocolException" errorValue
-        case parsedExc of
-          ProtocolException {error = err} ->
-            expectedError === err
-              >> pure exc
-          _ -> 
-            HUnit.assertFailure $ "Expected ProtocolException but got: " <> show parsedExc
+      Left exc@(ProtocolException {error = err}) -> do
+        expectedError === err
+        pure exc
       Left exc -> do
-        HUnit.assertFailure $ "Expected BiDIError but got: " <> show exc
+        HUnit.assertFailure $ "Expected ProtocolException but got: " <> show exc
       Right _ -> do
         HUnit.assertFailure $ "Expected ProtocolException with error " <> show expectedError <> " but action succeeded"
