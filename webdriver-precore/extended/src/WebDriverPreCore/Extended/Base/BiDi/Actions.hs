a61bccca75a1c1ac76fb38cae5189e51a510f840
John Walker <theghostjw@gmail.com>
2026-02-02 07:04:51 +1100

    Add actions modules

---
+++ webdriver-precore/extended/src/WebDriverPreCore/Extended/Base/BiDi/Actions.hs
@@ -0,0 +1,13 @@
+{-|
+Module: WebDriverPreCore.Extended.Base.BiDi.Actions
+Description: BiDi actions module
+-}
+module WebDriverPreCore.Extended.Base.BiDi.Actions
+  (
+  )
+where
+
+import WebDriverPreCore.Extended.Base.BiDi.API
+import WebDriverPreCore.Extended.Base.BiDi.Protocol
+
+
---
+++ webdriver-precore/extended/src/WebDriverPreCore/Extended/Base/HTTP/Actions.hs
@@ -0,0 +1,11 @@
+{-|
+Module: WebDriverPreCore.Extended.Base.HTTP.Actions
+Description: HTTP actions module
+-}
+module WebDriverPreCore.Extended.Base.HTTP.Actions
+  (
+  )
+where
+
+import WebDriverPreCore.Extended.Base.HTTP.API
+import WebDriverPreCore.Extended.Base.HTTP.Protocol
