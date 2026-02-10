abebdadef3c40208d40e285f9a1a3b14a6c86a74
John Walker <theghostjw@gmail.com>
2026-02-11 08:02:23 +1100

    wip

--- webdriver-precore/rio-poc/test/Test.hs
+++ webdriver-precore/rio-poc/test/Test.hs
@@ -24,6 +24,55 @@ tests =
     [ testCase "Basic Demo" basic_demo
     ]
 
+
+-- challenge 
+  -- basic bidi
+  -- basic http
+  -- bidi events
+  -- bidi and http mixed on the same instance
+  -- hooks to start runner and close connection
+  -- 
+  -- concurrency - 2 browsers
+
+  {-
+  import Test.Tasty
+import Test.Tasty.HUnit
+
+main :: IO ()
+main = do
+  defaultMain tests
+
+tests :: TestTree
+tests = withResource acquire release $ \getResource ->
+  testGroup "My Tests"
+    [ testCase "Test 1" $ do
+        resource <- getResource
+        -- use resource
+        
+    , testCase "Test 2" $ do
+        resource <- getResource
+        -- use same resource
+        
+    , testCase "Test 3" $ do
+        resource <- getResource
+        -- use same resource
+    ]
+
+-- Before hook: create resource
+acquire :: IO MyResource
+acquire = do
+  putStrLn "Setting up resource..."
+  -- create and return your resource
+  
+-- After hook: dispose resource
+release :: MyResource -> IO ()
+release resource = do
+  putStrLn "Cleaning up resource..."
+  -- dispose of resource
+  
+  
+  -}
+
 -- >>> basic_demo
 basic_demo :: IO ()
 basic_demo = do
