081c972477f35dd4de50f8846138cb8ec4f07017
John Walker <theghostjw@gmail.com>
2026-02-18 08:02:26 +1100

    wip

--- webdriver-precore/rio-poc/src/WebDriver/RIO/App.hs
+++ webdriver-precore/rio-poc/src/WebDriver/RIO/App.hs
@@ -8,18 +8,26 @@
 -- capabilities, then execute a RIO action in that context.
 module WebDriver.RIO.App
   ( runHttp,
-  -- withHttpSession,
-  -- newSession,
-  -- deleteSession,
+    withHttpSession,
+    exampleWithTimeouts,
+    WantWebDriverLogging (..),
+    defaultEndpoint,
   )
 where
 
 import RIO
-import RIO.Text (pack)
-import WebDriver.RIO.Env (BiDiEnv (..), HasHttpRunner, HttpEnv (..))
-import WebDriver.RIO.HTTP.Base.Actions (status)
+import WebDriver.RIO.Env
+  ( HasHttpRunner,
+    HttpEnv (..),
+    HttpSessionEnv (..),
+    getHttpRunner,
+    getLogger,
+  )
+import WebDriver.RIO.HTTP.Base.Actions (deleteSession, getTimeouts, setTimeouts, status)
 import WebDriver.RIO.Logging (LoggerConfig (..), withLogging)
-import WebDriverPreCore.HttpRunner (HttpEndpoint (..), HttpRunner, mkHttpRunner)
+import WebDriverPreCore.Extended.Capabilities qualified as EC
+import WebDriverPreCore.HTTP.Protocol (Timeouts (..))
+import WebDriverPreCore.HttpRunner (HttpEndpoint (..), HttpRunner (..), mkHttpRunner)
 
 data WantWebDriverLogging = WebDriverLogging | NoWebDriverLogging deriving (Eq, Show)
 
@@ -67,108 +75,64 @@ a = runHttp MkHttpEnv Console defaultEndpoint WebDriverLogging myAction
 
 -- | Create a session, run an action with it, then delete the session.
 --
--- Uses bracket to ensure the session is always cleaned up.
--- withHttpSession ::
---   (HasHttpRunner env, HasLogFunc env) =>
---   RIO HttpSessionEnv a ->
---   RIO env a
--- withHttpSession action = do
---   httpRunner <- getHttpRunner
---   logFunc <- getLogger
---   bracket
---     ((.sessionId) <$> newSession)
---     (deleteSession)
---     (\httpSessionId -> runRIO (MkHttpSessionEnv {logFunc, httpRunner, httpSessionId}) action)
+-- Uses bracket to ensure the session is always cleaned up even if the action fails.
+withHttpSession ::
+  (HasHttpRunner env, HasLogFunc env) =>
+  EC.HttpCapabilities ->
+  -- ^ Capabilities to request for the session
+  RIO HttpSessionEnv a ->
+  -- ^ Action to run with the session
+  RIO env a
+withHttpSession caps action = do
+  httpRunner@MkHttpRunner{run} <- getHttpRunner
+  lf <- getLogger
+  bracket
+    (liftIO $ do 
+       EC.newHttpSession run caps
+       here)
+    (\(EC.MkHttpSessionResponse {sessionId = sid}) -> do
+        let sessionEnv = MkHttpSessionEnv {logFunc = lf, httpRunner, httpSessionId = sid.id}
+        liftIO $ runRIO sessionEnv deleteSession)
+    (\(EC.MkHttpSessionResponse {sessionId = sid}) -> do
+        let sessionEnv = MkHttpSessionEnv {logFunc = lf, httpRunner, httpSessionId = sid.id}
+        runRIO sessionEnv action)
 
--- withHttpSession
--- runBiDi :: LoggerConfig -> BiDiCapabilities -> RIO BiDiEnv a -> IO a
--- runBiDi loggerConfig bidiCaps bidiAction = undefined
---    withLogging loggerConfig $ \lf -> do
---     runRIO (MkLoggerEnv lf) $ do
---       let socketLogger = Just logInfo
---       bracket
---   let biDiEnv = MkBiDiEnv lf bidiCaps
---   runRIO biDiEnv action
+-- | Example showing how to use withHttpSession to set and get timeouts
+exampleWithTimeouts :: IO ()
+exampleWithTimeouts = runHttp MkHttpEnv Console defaultEndpoint WebDriverLogging $ do
+  -- Define minimal capabilities
+  let caps =
+        EC.MkFullCapabilitiesRequest
+          { alwaysMatch =
+              Just $
+                EC.MkHttpCapability
+                  { browserName = Just EC.Firefox,
+                    platformName = Nothing,
+                    acceptInsecureCerts = False,
+                    pageLoadStrategy = Nothing,
+                    proxy = Nothing,
+                    timeouts = Nothing,
+                    strictFileInteractability = Nothing,
+                    unhandledPromptBehavior = Nothing,
+                    httpWebSocketUrl = Nothing,
+                    vendorSpecific = Nothing
+                  },
+            firstMatch = []
+          }
 
--- | Run a BiDi session with typed commands
--- withBiDi
---   :: Maybe (Text -> IO ())  -- ^ Optional logger>
---   -> BiDiUrl
---   -> (BiDiRunner -> IO ())
---   -> IO ()
--- withBiDi mLogger bidiUrl action =
---   withBiDiBase mLogger bidiUrl $ \sa ->
---     action (mkBiDiRunner sa)
+  -- Create a session, run actions, and automatically clean up
+  withHttpSession caps $ do
+    logInfo "Session created, setting timeouts"
 
--- -- | Initialize a BaseEnv context and run a RIO action.
--- --
--- -- Takes logger config and capabilities, creates a BaseEnv, logs a debug
--- -- message, then executes the provided action.
--- runWebDriver :: LoggerConfig -> FullCapabilitiesRequest -> RIO BaseEnv a -> IO a
--- runWebDriver loggerConfig caps action =
---   withLogging loggerConfig $ \lf -> do
---     let env = MkBaseEnv lf caps Nothing
---     runRIO env $ do
---       logDebug "Running action"
---       action
+    -- Set new timeout values
+    let newTimeouts =
+          MkTimeouts
+            { implicit = Just 5000, -- 5 seconds
+              pageLoad = Just 60000, -- 60 seconds
+              script = Just 30000 -- 30 seconds
+            }
+    setTimeouts newTimeouts
 
--- -- | Run an action in BiDiEnv context with the given runner.
--- --
--- -- Extracts LogFunc and capabilities from the current environment, constructs
--- -- a BiDiEnv with the given runner, then executes the provided action.
--- runBiDi :: BiDiRunner -> RIO BiDiEnv a -> RIO BaseEnv a
--- runBiDi runner action = do
---   caps <- getCapabilities
---   mCapsResp <- getCapabilitiesResponse
---   lf <- getLogger
---   let biDiEnv = MkBiDiEnv lf caps mCapsResp runner
---   runRIO biDiEnv action
-
--- -- | Create a BiDiRunner from capabilities and run an action in BiDiEnv context.
---
--- Extracts logger and capabilities from BaseEnv, creates a BiDiRunner using
--- withBiDi from the BiDiRunner module, then executes the action in a BiDiEnv
--- context. The BiDi URL comes from the webSocketUrl in the capabilities response.
--- -- Uses finally to ensure cleanup happens evRIO BaseEnven if the action fails.
-
--- withBiDiRunner ::
---   (BiDiRunner -> IO ()) ->
---   RIO BiDiEnv () ->
---   RIO BaseEnv ()
--- withBiDiRunner cleanup action = do
---   caps <- getCapabilities
---   mCapsResp <- getCapabilitiesResponse
---   capsResp <- case mCapsResp of
---     Nothing -> error "No capabilities response available. Create a session first."
---     Just cr -> pure cr
---   bidiUrl <- getBiDiUrl capsResp
---   lf <- getLogger
-
---   -- Create logger function from RIO logInfo
---   let logger txt = runRIO (MkBaseEnv lf caps mCapsResp) $ logInfo $ display txt
-
---   liftIO $ BiDiRunner.withBiDi (Just logger) bidiUrl $ \runner -> do
---     finally
---       (runRIO (MkBaseEnv lf caps mCapsResp) $ runBiDi runner action)
---       (cleanup runner)
-
--- -- | Extract BiDi URL from capabilities response
--- getBiDiUrl :: (MonadIO m) => SessionResponse -> m BiDiUrl
--- getBiDiUrl capsResp = case capsResp of
---   BiDiSessionResponse {bidiCapabilities = MkBiDiCapabilities {bidiWebSocketUrl = Just wsUrl}} ->
---     case parseBiDiUrl wsUrl of
---       Just url -> pure url
---       Nothing -> error $ "Failed to parse WebSocket URL: " <> show wsUrl
---   BiDiSessionResponse {bidiCapabilities = MkBiDiCapabilities {bidiWebSocketUrl = Nothing}} ->
---     error "No WebSocket URL in BiDi capabilities response"
---   HttpSessionResponse {} ->
---     error "Cannot get BiDi URL from HTTP capabilities response"
-
--- getCapabilities :: HasCapabilities env => RIO env FullCapabilitiesRequest
--- getCapabilities = view capabilitiesL
-
--- getCapabilitiesResponse :: HasCapabilitiesResponse env => RIO env (Maybe SessionResponse)
--- getCapabilitiesResponse = view capabilitiesResponseL
-
--- getLogger :: HasLogFunc env => RIO env LogFunc
--- getLogger = view logFuncL
+    -- Get and log the current timeouts
+    currentTimeouts <- getTimeouts
+    logInfo $ "Current timeouts: " <> displayShow currentTimeouts
