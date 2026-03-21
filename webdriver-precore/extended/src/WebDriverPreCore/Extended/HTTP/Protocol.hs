e4956d4126cbe5515297dbe8eddc992dba0e3048
John Walker <theghostjw@gmail.com>
2026-03-21 11:25:25 +1100

    more moving stuff around

--- webdriver-precore/extended/src/WebDriverPreCore/Extended/HTTP/Protocol.hs
+++ webdriver-precore/extended/src/WebDriverPreCore/Extended/HTTP/Protocol.hs
@@ -1,24 +1,15 @@
-{-|
-Module: WebDriverPreCore.Extended.HTTP.Protocol
-Description: Extended WebDriver HTTP API with convenience functions
-
-This module re-exports the HTTP extended API along with common utilities.
-
-- "WebDriverPreCore.Extended.HTTP.Base.API" - HTTP API commands
-- "WebDriverPreCore.Extended.HTTP.Base.Protocol" - HTTP protocol types
-- "WebDriverPreCore.Utils.Timeout" - Timeout utilities
-- "WebDriverPreCore.Error" - Error types
--}
+-- |
+-- Module: WebDriverPreCore.Extended.HTTP.Protocol
+-- Description: Extended WebDriver HTTP API with convenience functions
+--
+-- This module re-exports the HTTP extended API along with common utilities.
+--
+-- - "WebDriverPreCore.Extended.HTTP.Base.API" - HTTP API commands
+-- - "WebDriverPreCore.Extended.HTTP.Base.Protocol" - HTTP protocol types
+-- - "WebDriverPreCore.Utils.Timeout" - Timeout utilities
+-- - "WebDriverPreCore.Error" - Error types
 module WebDriverPreCore.Extended.HTTP.Protocol
-  ( -- * HTTP API (re-exported)
-    module WebDriverPreCore.Extended.HTTP.Base.API,
-    -- * Utils (re-exported)
-    module WebDriverPreCore.Utils.Timeout,
-    -- * Error (re-exported)
-    module WebDriverPreCore.Error,
+  ( 
   )
 where
 
-import WebDriverPreCore.Error
-import WebDriverPreCore.Extended.HTTP.Base.API
-import WebDriverPreCore.Utils.Timeout
---
+++ webdriver-precore/extended/src/WebDriverPreCore/Extended/Protocol.hs
@@ -0,0 +1,12 @@
+module WebDriverPreCore.Extended.Protocol
+  ( module WebDriverPreCore.Extended.Capabilities,
+    module WebDriverPreCore.Error,
+    module WebDriverPreCore.Extended.Locators,
+    module WebDriverPreCore.Utils.Timeout,
+  )
+where
+
+import WebDriverPreCore.Error
+import WebDriverPreCore.Extended.Capabilities
+import WebDriverPreCore.Extended.Locators
+import WebDriverPreCore.Utils.Timeout
