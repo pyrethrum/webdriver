561b0b70fd6d3a8c74aa287cc57d592f9adb2de3
John Walker <theghostjw@gmail.com>
2026-03-27 17:44:03 +1100

    wip

--- webdriver-precore/extended/src/WebDriverPreCore/Extended/Locate.hs
+++ webdriver-precore/extended/src/WebDriverPreCore/Extended/Locate.hs
@@ -2,27 +2,27 @@ module WebDriverPreCore.Extended.Locate
   ( LocateException (..),
     Cardinality (..),
     LocateOps (..),
-    locateHttp
+    locateHttp,
   )
 where
 
 import Control.Exception (Exception, SomeException)
+import Data.Function ((&))
 import Data.Text
+import GHC.Stack (HasCallStack)
 import WebDriverPreCore.Extended.HTTP.Base.Actions
 import WebDriverPreCore.Extended.HTTP.Base.Protocol (ElementId, Session)
 import WebDriverPreCore.Extended.HTTP.Internal (Runner)
-import WebDriverPreCore.Extended.Locators.Internal (Locator, prepare, Protocol)
+import WebDriverPreCore.Extended.Locators.Internal (Locator, Protocol)
 import WebDriverPreCore.Extended.Locators.Internal qualified as LI
+import WebDriverPreCore.Extended.SimplifiedLocator.Internal (SimplifiedLocator (..), prepareSimplify)
 import WebDriverPreCore.HTTP.Protocol (Command, Selector)
-import WebDriverPreCore.Extended.SimplifiedLocator.Internal (SimplifiedLocator(..), prepareSimplify)
-import GHC.Stack (HasCallStack)
-import Data.Function ((&))
 
 data LocateException
   = AmbiguousLocateResult {description :: Text}
   | InvalidLocator LI.InvalidLocator
   | WebDriverException SomeException
-  deriving Show
+  deriving (Show)
 
 instance Exception LocateException
 
@@ -40,13 +40,14 @@ data LocateOps = MkLocateOps
 -- findElements :: forall m. Runner m [ElementId] -> Session -> Selector -> m [ElementId]
 
 locateHttp ::
-  forall m. (Functor m, Applicative m )=>
+  forall m.
+  (Monad m) =>
   -- | throw exceptions
   (forall a e. (HasCallStack, Exception e) => e -> m a) ->
   -- | catch exceptions
   (forall a e. (HasCallStack, Exception e) => m a -> (e -> m a) -> m a) ->
   -- | runner
-  (forall b. Command b -> m b)->
+  (forall b. Command b -> m b) ->
   -- | default locator
   (Text -> Locator) ->
   -- | session
@@ -54,31 +55,44 @@ locateHttp ::
   -- | locate ops
   LocateOps ->
   -- | locator
-  Locator -> 
-  m (Either LocateException ElementId)
-locateHttp throw catch runner defLoc ses ops loc = 
-    preparedLoc & 
-      either 
-        (throw . InvalidLocator) 
-        \loc -> undefined
-   where
-    preparedLoc = prepareSimplify defLoc ops.protocol loc
+  Locator ->
+  m ElementId
+locateHttp throw catch runner defLoc ses MkLocateOps {cardinality, protocol} loc =
+  preparedLoc
+    & either
+      (throw . InvalidLocator)
+      \loc -> do
+        case cardinality of
+          Unique -> findElm loc
+          First -> findElm loc
+          Many ->
+            findElms loc >>= \case
+              Left err -> pure $ Left err
+              Right [] -> pure $ Left $ WebDriverException $ toException $ userError "Expected at least one element, but found none."
+              Right (x : xs) -> case cardinality of
+                Unique -> undefined
+                  -- if null xs
+                  --   then pure $ Right x
+                  --   else pure $ Left $ AmbiguousLocateResult $ "Expected exactly one element, but found " <> pack (show (1 + length xs)) <> "."
+                First -> pure $ Right x
+                Many -> pure $ Right x -- TODO: return all elements, not just the first one
+  where
+    preparedLoc = prepareSimplify defLoc protocol loc
 
-    elmFind :: forall a. ((Command a -> m a) -> Session -> Selector -> m a) -> Selector -> m (Either LocateException a)
-    elmFind f sel = 
-      catch 
-        (Right <$> f runner ses sel)
-        (pure . Left . WebDriverException)
+    runLocate :: forall a. ((Command a -> m a) -> Session -> Selector -> m a) -> Selector -> m a
+    runLocate f sel =
+      catch
+        (f runner ses sel)
+        (throw . WebDriverException)
 
-    findElm :: Selector -> m (Either LocateException ElementId)  
-    findElm = elmFind findElement 
+    findElm :: Selector -> m ElementId
+    findElm = runLocate findElement
 
+    findElms :: Selector -> m [ElementId]
+    findElms = runLocate findElements
 
-    findElms :: Selector -> m (Either LocateException [ElementId])
-    findElms = elmFind findElements
-
-    locate :: SimplifiedLocator -> m (Either LocateException ElementId)
-    locate = \case
+    httpLocate :: SimplifiedLocator -> m ElementId
+    httpLocate = \case
       CSS {} -> undefined
       XPath {} -> undefined
       Role {} -> undefined
@@ -89,4 +103,3 @@ locateHttp throw catch runner defLoc ses ops loc =
       Any {} -> undefined
       None {} -> undefined
       PostFilter _ -> undefined
-
